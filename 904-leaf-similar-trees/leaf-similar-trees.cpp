b43b8ccc32ec99216e86587779b16a89908d0963
kushal Agarwal <118652910+kushalag02@users.noreply.github.com>
2024-05-19 12:41:18 +0530

    Time: 2 ms (63.18%) | Memory: 14.2 MB (86.80%) - LeetSync

---
+++ 904-leaf-similar-trees/leaf-similar-trees.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    void findLeaf(TreeNode* root, vector<int>& leaf) {
+        if (root == NULL)
+            return;
+        if (root->left == NULL && root->right == NULL) {
+            leaf.push_back(root->val);
+        }
+        findLeaf(root->left, leaf);
+        findLeaf(root->right, leaf);
+    }
+    vector<int> makeArray(TreeNode* root) {
+        vector<int> leafNode;
+        findLeaf(root, leafNode);
+        return leafNode;
+    }
+    bool leafSimilar(TreeNode* root1, TreeNode* root2) {
+        vector<int> A;
+        vector<int> B;
+        A = makeArray(root1);
+        B = makeArray(root2);
+        if (A.size() != B.size())
+            return false;
+        for (int i = 0; i < A.size(); i++) {
+            if (A[i] != B[i])
+                return false;
+        }
+        return true;
+    }
+};
