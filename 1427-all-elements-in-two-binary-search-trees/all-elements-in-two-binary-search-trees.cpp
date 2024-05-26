c107e49be6670c5b78b88f33fec9eeddb9856123
kushal Agarwal <118652910+kushalag02@users.noreply.github.com>
2024-05-26 18:00:44 +0530

    Time: 99 ms (81.40%) | Memory: 84.8 MB (44.65%) - LeetSync

---
+++ 1427-all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
+        vector<int> A;
+        vector<int> B;
+        makeVector(root1, A);
+        makeVector(root2, B);
+        int n = A.size();
+        int m = B.size();
+        vector<int> ans(n + m);
+        for (int i = 0; i < n; i++) {
+            ans[i] = A[i];
+        }
+        for (int i = 0; i < m; i++) {
+            ans[i + n] = B[i];
+        }
+        sort(begin(ans), end(ans));
+        return ans;
+    }
+    void makeVector(TreeNode* root, vector<int>& A) {
+        if (root == nullptr)
+            return;
+        makeVector(root->left, A);
+        A.push_back(root->val);
+        makeVector(root->right, A);
+    }
+};
