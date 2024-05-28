9601df9046a879e510e8bc1457f3e40c682c4c8a
kushal Agarwal <118652910+kushalag02@users.noreply.github.com>
2024-05-28 13:00:32 +0530

    Time: 0 ms (100.00%) | Memory: 7.2 MB (22.62%) - LeetSync

---
+++ 371-sum-of-two-integers/sum-of-two-integers.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int getSum(int a, int b) {
+        while (b > 0) {
+            a++; 
+            b--;
+        }
+        while (b < 0) {
+            a--;
+            b++;
+        }
+
+        return a;
+    }
+};
