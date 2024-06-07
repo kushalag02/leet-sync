0587ec653ca68df3d5b8adaad096082c29991269
kushal Agarwal <118652910+kushalag02@users.noreply.github.com>
2024-06-07 21:37:06 +0530

    Time: 7 ms (79.95%) | Memory: 14.7 MB (93.29%) - LeetSync

--- 2331-intersection-of-multiple-arrays/intersection-of-multiple-arrays.cpp
+++ 2331-intersection-of-multiple-arrays/intersection-of-multiple-arrays.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     vector<int> intersection(vector<vector<int>>& nums) {
-        int n = nums.size();
-        vector<int> A(1001, 0);
-
-        for (int i = 0; i < n; i++) {
-            for (auto it : nums[i]) {
-                A[it]++;
+        vector<int> result = nums[0];
+        for (int i = 1; i < nums.size(); i++) {
+            vector<int> temp;
+            vector<int> curr = nums[i];
+            sort(curr.begin(), curr.end());
+            for (int num : result) {
+                auto it = find(curr.begin(), curr.end(), num);
+                if (it != curr.end()) {
+                    temp.push_back(num);
+                    curr.erase(it);
+                }
             }
+            result = temp;
         }
-        vector<int> count;
-        for (int i = 1; i <= 1000; i++) {
-            if (A[i] == n) {
-                count.push_back(i);
-            }
-        }
-
-        return count;
+        sort(result.begin(), result.end());
+        return result;
     }
 };
