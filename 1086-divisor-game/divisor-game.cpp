1787afb150d946691bdb5c76613161cf4da81758
kushal Agarwal <118652910+kushalag02@users.noreply.github.com>
2024-06-12 12:28:48 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (13.00%) - LeetSync

--- 1086-divisor-game/divisor-game.cpp
+++ 1086-divisor-game/divisor-game.cpp
@@ -1,9 +1,20 @@
 class Solution {
 public:
     bool divisorGame(int n) {
-        if (n % 2)
-            return false;
-        else
-            return true;
+        // if (n % 2)
+        //     return false;
+        // else
+        //     return true;
+        vector<bool> dp(n + 1);
+        dp[0] = false;
+        dp[1] = false;
+        dp[2] = true;
+        for (int i = 3; i <= n; i++) {
+            for (int j = 1; j * j < i; j++) {
+                if (i % j == 0 && dp[i - j] == false)
+                    dp[i] = true;
+            }
+        }
+        return dp[n];
     }
 };
