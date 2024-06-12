1d2dcac46e8e53c4f3464eb109d7386d2d208f75
kushal Agarwal <118652910+kushalag02@users.noreply.github.com>
2024-06-12 18:25:20 +0530

    Time: 0 ms (100.00%) | Memory: 7.2 MB (11.17%) - LeetSync

--- 357-count-numbers-with-unique-digits/count-numbers-with-unique-digits.cpp
+++ 357-count-numbers-with-unique-digits/count-numbers-with-unique-digits.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
-    int solve(int n, vector<int>& dp) {
-        if (dp[n] != -1)
-            return dp[n];
-        int unique = 9;
-        int digits = 9;
-        for (int i = 1; i < n; i++) {
-            unique *= digits;
-            digits--;
-        }
-        return dp[n] = solve(n - 1, dp) + unique;
-    }
     int countNumbersWithUniqueDigits(int n) {
-        if (n == 0)
-            return 1;
-        if (n == 1)
-            return 10;
-        vector<int> dp(n + 1, -1);
+      // Tabulation
+      if(n==0)return 1;
+        vector<int> dp(n + 1, 0);
         dp[0] = 1;
         dp[1] = 10;
-        return solve(n, dp);
+        for (int i = 2; i <= n; i++) {
+            int unique = 9;
+            int digits = 9;
+
+            for (int j = 1; j < i; j++) {
+                unique *= digits;
+                digits--;
+            }
+
+            dp[i] = dp[i - 1] + unique;
+        }
+        return dp[n];
     }
 };
