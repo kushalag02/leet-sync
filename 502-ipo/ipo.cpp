07e8000603517d4eccfdaabaa01780a82394ca46
kushal Agarwal <118652910+kushalag02@users.noreply.github.com>
2024-06-15 22:41:37 +0530

    Time: 130 ms (87.46%) | Memory: 81.7 MB (78.96%) - LeetSync

--- 502-ipo/ipo.cpp
+++ 502-ipo/ipo.cpp
@@ -1,6 +1,3 @@
-bool comparator(const pair<int, int>& a, const pair<int, int>& b) {
-    return a.second < b.second;
-}
 class Solution {
 public:
     int findMaximizedCapital(int k, int w, vector<int>& profits,
@@ -8,14 +5,14 @@ public:
         int n = profits.size();
         vector<pair<int, int>> vp(n);
         for (int i = 0; i < n; i++) {
-            vp[i] = {profits[i], capital[i]};
+            vp[i] = {capital[i], profits[i]};
         }
-        sort(vp.begin(), vp.end(), comparator);
+        sort(vp.begin(), vp.end());
         priority_queue<int> pq;
         int i = 0;
         while (k--) {
-            while (i < n && vp[i].second <= w) {
-                pq.push(vp[i].first);
+            while (i < n && vp[i].first <= w) {
+                pq.push(vp[i].second);
                 i++;
             }
             if (pq.empty()) {
