e041a17010640529fc124526aaca11ee017eaeaa
kushal Agarwal <118652910+kushalag02@users.noreply.github.com>
2024-06-21 23:22:41 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (95.91%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,25 +1,13 @@
 class Solution {
 public:
     int change(int amount, vector<int>& coins) {
-        vector<vector<int>> dp(coins.size(), vector<int>(amount + 1, -1));
-        return solve(amount, coins, 0, dp);
-    }
-
-    int solve(int amount, vector<int>& coins, int index,
-              vector<vector<int>>& dp) {
-        if (amount == 0)
-            return 1;
-
-        if (amount < 0 || index == coins.size())
-            return 0;
-
-        if (dp[index][amount] != -1)
-            return dp[index][amount];
-
-        int inc = solve(amount - coins[index], coins, index, dp);
-        int exc = solve(amount, coins, index + 1, dp);
-
-        dp[index][amount] = inc + exc;
-        return dp[index][amount];
+        vector<int> dp(amount + 1, 0);
+        dp[0] = 1;
+        for (int coin : coins) {
+            for (int i = coin; i <= amount; i++) {
+                dp[i] += dp[i - coin];
+            }
+        }
+        return dp[amount];
     }
 };
