504a1aea3007e4c111ffaee0f2035240b6c22bba
kushal Agarwal <118652910+kushalag02@users.noreply.github.com>
2024-06-25 12:08:06 +0530

    Time: 19 ms (94.02%) | Memory: 33.4 MB (16.12%) - LeetSync

---
+++ 538-convert-bst-to-greater-tree/convert-bst-to-greater-tree.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    void inorder(TreeNode* root, vector<int>& result) {
+        if (root == NULL) {
+            return;
+        }
+        inorder(root->left, result);
+        result.push_back(root->val);
+        inorder(root->right, result);
+    }
+    void solve(TreeNode* root, vector<int>& result, int& idx) {
+        if (root == NULL)
+            return;
+        solve(root->left, result, idx);
+        root->val = result[idx];
+        idx++;
+        solve(root->right, result, idx);
+    }
+    TreeNode* convertBST(TreeNode* root) {
+        vector<int> result;
+        inorder(root, result);
+        int n = result.size();
+        for (int i = n - 2; i >= 0; i--) {
+            result[i] += result[i + 1];
+        }
+        int idx = 0;
+        solve(root, result, idx);
+        return root;
+    }
+};
