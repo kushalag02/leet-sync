8db0e275609229912c1d4f2fe486c90b36d4f7aa
kushal Agarwal <118652910+kushalag02@users.noreply.github.com>
2024-07-09 00:46:44 +0530

    Time: 219 ms (70.26%) | Memory: 61.7 MB (72.25%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
@@ -1,46 +1,65 @@
 class Solution {
 public:
-    int minCostConnectPoints(vector<vector<int>>& points) {
-      // Prims Algo
-        int n = points.size();
-        vector<pair<int, int>> adj[n];
+    vector<int> parent;
+    vector<int> rank;
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i == j)
-                    continue;
-                int w = abs(points[i][0] - points[j][0]) +
-                        abs(points[i][1] - points[j][1]);
-                adj[i].push_back({j, w});
+    int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+
+    void unionSets(int a, int b) {
+        int aRoot = find(a);
+        int bRoot = find(b);
+        if (aRoot != bRoot) {
+            if (rank[aRoot] < rank[bRoot]) {
+                parent[aRoot] = bRoot;
+            } else if (rank[aRoot] > rank[bRoot]) {
+                parent[bRoot] = aRoot;
+            } else {
+                parent[aRoot] = bRoot;
+                rank[bRoot]++;
             }
         }
+    }
+
+    int minCostConnectPoints(vector<vector<int>>& points) {
+        int n = points.size();
+        parent.resize(n);
+        rank.resize(n, 0);
+
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+
+        vector<pair<int, pair<int, int>>> edges; //{ weight, {p1, p2}}
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int weight = abs(points[i][0] - points[j][0]) +
+                             abs(points[i][1] - points[j][1]);
+                edges.push_back({weight, {i, j}});
+            }
+        }
+        sort(edges.begin(), edges.end());
 
         int ans = 0;
-        priority_queue<pair<int, int>, vector<pair<int, int>>,
-                       greater<pair<int, int>>>
-            pq;
-        pq.push({0, 0});
-        vector<bool> vis(n, false);
-        while (!pq.empty()) {
-            int node = pq.top().second;
-            int dist = pq.top().first;
-            pq.pop();
-
-            if (vis[node] == true) {
-                continue;
-            }
-            vis[node] = true;
-            ans += dist;
-
-            for (auto neighbour : adj[node]) {
-                int adjNode = neighbour.first;
-                int distance = neighbour.second;
-
-                if (!vis[adjNode]) {
-                    pq.push({distance, adjNode});
+        int numEdges = 0;
+        for (auto it : edges) {
+            int weight = it.first;
+            int u = it.second.first;
+            int v = it.second.second;
+            if (find(u) != find(v)) {
+                unionSets(u, v);
+                ans += weight;
+                numEdges++;
+                if (numEdges == n - 1) {
+                    break;
                 }
             }
         }
+
         return ans;
     }
 };
